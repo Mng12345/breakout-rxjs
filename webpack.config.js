bd599ca66f300aedb4cd847f966c97c62cee0e91
张铭 <mingzhang@chutianyun.gov.cn>
2021-02-12 19:19:31 +0800

    s

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, ''),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
     open: true,
